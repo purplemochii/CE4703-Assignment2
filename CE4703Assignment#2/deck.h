d20d4599d8be253e086d373a131185fba838258f
Shrestha Dey <24017795@studentmail.ul.ie>
2025-11-25 12:58:06 +0000

    Add deck.h to project and update project files
    Included deck.h in CE4703Assignment#2.vcxproj and
    CE4703Assignment#2.vcxproj.filters to organize it under
    "Header Files". Created deck.h with documentation, defining
    a card data type and declaring helper functions. Added
    include guards and included card.h.

--- CE4703Assignment#2/deck.h
+++ CE4703Assignment#2/deck.h
@@ -1,1 +1,28 @@
+/**
+ * @file deck.h
+ * @brief //Card type: Suit enum, Rank enum, Card struct and basic operations.
+ *
+ * This file defines the data type for a single playing card.
+ * A card has a suit (Club, Spade, Heart, Diamond)
+ * and a rank (Two through Ace). It also declares helper
+ * functions to print and compare cards.
+ * This type will be used in the card game program.
+ *
+ * @author Shrestha Dey
+ * @date 25 November 2025
+ */
+
 #pragma once
+#ifndef DECK_H
+#define DECK_H
+
+#include card.h
+
+/**
+* @brief 
+*/
+typedef struct {
+
+} CardDeck;
+
+#endif
