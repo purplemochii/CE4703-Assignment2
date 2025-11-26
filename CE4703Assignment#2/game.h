9ecb1d6277307c74426066d05cc0cc8b653294c1
mxchii <kiki.dollarh@gmail.com>
2025-11-26 11:44:04 +0000

    added game.h, player struct, gamestate, and method headers as well. no doxygen i'll do that lot later🙏🏾

---
+++ CE4703Assignment#2/game.h
@@ -0,0 +1,46 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include "deck.h"
+#include "sort.h"
+
+#define NUM_PLAYERS 2
+#define INITIAL_HAND_SIZE 8
+#define MAX_HAND_SIZE 20
+
+/* --- player --- */
+typedef struct {
+	CardDeck* hand; // player's hand
+	char name[20]; // player
+} Player;
+
+/* --- game state --- */
+typedef struct {
+	Player players[NUM_PLAYERS]; // players
+	CardDeck* hiddenDeck;	// draw pile (the ones that are face down)
+	CardDeck* palyedDeck;	// discarded pile (faced up)
+	Card currentcard;	// current card that is being matched
+	int currentPlayer;	// player number (either 0 or 1 bc theres only 2 players)
+	int gameOver;	 // flag that signifies game over
+} GameState;
+
+/* --- game init and cleanup --- */
+GameState* initGame(int numPacks);
+void freeGame(GameState* game);
+
+/* --- game op ---*/
+void dealInitialCards(GameState* game);
+int findMatchingCard(const GameState* game, int playerIndex);
+int playCard(GameState* game, int playerIndex, int cardIndex);
+void drawCardForPlayer(GameState* game, int playerIndex);
+void reshuffleHiddenDeck(GameState* game);
+
+/* --- display fns ---*/
+void printPlayerHand(int playerIndex, const CardDeck* hand);
+void printGameState(const GameState* game);
+
+/* --- game flow ---*/
+void nextTurn(GameState* game);
+int checkGameOver(const GameState* game);
+
+#endif !GAME_H
